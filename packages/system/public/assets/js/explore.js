4ee83dc6c43d57af4e606b334bdc1986911b1110
zweipix <316787983@qq.com>
2014-11-30 15:02:56 +0800

    person界面无信息图片

--- packages/system/public/assets/js/explore.js
+++ packages/system/public/assets/js/explore.js
@@ -3,9 +3,10 @@
  */
 
 $(window).on('load', function() {
-    $('')
+    $('.to-sub').on('click', subChannelAjax(event));
 });
 
 function subChannelAjax(event) {
-    console.log(event);
+    console.log(event.currentTarget);
+
 }
