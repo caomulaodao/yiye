61af26d45f046973ead2587d982416cbec7c75bc
zweipix <zweipix@yiye.me>
2014-11-30 12:23:41 +0800

    explore页面修改

--- packages/system/public/assets/js/explore.js
+++ packages/system/public/assets/js/explore.js
@@ -3,25 +3,6 @@
  */
 
 $(window).on('load', function() {
-    mainWidth();
-    
+
 });
 
-function mainWidth() {
-    var $oParent = $('#water-fall-main');
-    var $oBoxW = $('.water-fall-box').width()+20;
-
-    //计算瀑布流显示的列数
-    var cols = Math.floor($(window).width() / $oBoxW);
-    var rows = Math.ceil($('.water-fall-box').length / cols);
-
-    //设置main的宽度
-    var mainWidth = 350 * cols +'px';
-    $oParent.css('width', mainWidth);
-    console.log(mainWidth);
-
-    //设置main的高度
-    var mainHeight = 381 * rows + 'px';
-    $oParent.css('height', mainHeight);
-    console.log(mainHeight);
-}
