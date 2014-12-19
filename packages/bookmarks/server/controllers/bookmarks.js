c02cd8502cf576e4ed01a113ce15394eb4cf0071
koi <472617147@qq.com>
2014-12-19 20:38:25 +0800

    bug

--- packages/bookmarks/server/controllers/bookmarks.js
+++ packages/bookmarks/server/controllers/bookmarks.js
@@ -299,7 +299,7 @@ exports.init  =  function(req,res){
 exports.like = function(req,res){
 
     if(!req.user) return res.sendResult('请先注册或登录',1000,null);
-    var bookmarkId = req.body['bookmarkId']; console.log(bookmarkId);
+    var bookmarkId = req.body['bookmarkId'];
     if(!verify.idVerify(bookmarkId)){return res.sendResult('参数类型错误',2000,null)}
     async.parallel({
         isHated:function(callback){
@@ -332,7 +332,7 @@ exports.like = function(req,res){
                         like.channelName = bookmark.channelInfo.channelName,
                         like.bookmarkLogo = bookmark.image,
                         like.bookmarkName = bookmark.title,
-                        like.userLogo = req.user.userLogo,
+                        like.userLogo = req.user.avatar,
                         //保存点赞数据
                         like.save(function (err) {
                             if (err) {console.log(err);return res.sendError()}
