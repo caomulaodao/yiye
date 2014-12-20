702a73c62eacbed00ae6b1fda6581762b8ad8369
caomulaodao <huanglaodao@Gmail.com>
2014-12-20 18:59:25 +0800

    初始化后台

--- packages/overlook/server/controllers/overlook.js
+++ packages/overlook/server/controllers/overlook.js
@@ -4,9 +4,16 @@ var mongoose = require('mongoose'),
     xss =require('xss'),
     moment = require('moment'),
     User = mongoose.model('User'),
-    tool = require('../../../../config/tools/tool');
+    tool = require('../../../../config/tools/tool'),
     Channel2User = mongoose.model('Channel2User'),
     Channels = mongoose.model('Channels'),
     Bookmarks = mongoose.model('Bookmarks'),
-    BookmarkLike = mongoose.model('BookmarkLike');
+    BookmarkLike = mongoose.model('BookmarkLike'),
     Myverify = require('../../../../config/tools/verify');
+
+exports.renderMain = function(req,res,Package){
+    Package.render('index',{}, function(err, html) {
+        if(err) {console.log(err);return res.sendError()}
+        res.send(html);
+    });
+}
--- packages/overlook/server/routes/overlook.js
+++ packages/overlook/server/routes/overlook.js
@@ -1,26 +1,12 @@
 'use strict';
 
+var overlook = require('../controllers/overlook.js');
+
+
 // The Package is past automatically as first parameter
 module.exports = function(Overlook, app, auth, database) {
-
-  app.get('/overlook/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
-
-  app.get('/overlook/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
-
-  app.get('/overlook/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
-
-  app.get('/overlook/example/render', function(req, res, next) {
-    Overlook.render('index', {
-      package: 'overlook'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
+    //获取个人主页
+    app.get('/overlook', function(req, res, next) {
+        overlook.renderMain(req,res,Overlook);
     });
-  });
 };
